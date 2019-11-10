c16a8c45abaff2c4903925a6f4089f123625b601
rodrigues882013 <rodrigues882007@gmail.com>
2019-11-10 00:55:01 -0300

    -- adding nginx wrapping up all stuffs and select a model to build frontend.

--- frontend/helpers/constants.js
+++ frontend/helpers/constants.js
@@ -1,8 +1,3 @@
 export default {
-  WP_PHONE_NUMBER: '5521964668637',
-  CALL_PHONE_NUMBER: '5521969551168',
-  EMAIL: 'rmservicostransportes@gmail.com',
-  LEGAL_PERSON: 'Pessoa jurídica',
-  HUMAN: 'pessoafísica',
   API: 'http://localhost:3000/api/v1'
 }
